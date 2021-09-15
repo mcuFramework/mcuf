c4c24bf39c8f9b5a95b0966a08496fb12486dac0
ZxyKira <danny41553@gmail.com>
2021-09-16 00:02:34 +0800

    [add] macro message

--- src/main/mcuf_macro.h
+++ src/main/mcuf_macro.h
@@ -11,6 +11,11 @@
 /* ****************************************************************************************
  * Macro
  */
+ 
+#define MESSAGE_OUT_OF_ENUM "Out of enum."
+#define MESSAGE_OUT_OF_MEMORY "Out of memory."
+#define MESSAGE_OUT_OF_RANGE "Out of range."
+#define MESSAGE_NULL_POINTER "Null pointer."
 
 #define MCUF_DEBUG
 
@@ -18,8 +23,8 @@
   #define INFO(message) mcuf::lang::System::info(__PRETTY_FUNCTION__, message)
   #define THROW_ERROR(message) mcuf::lang::System::throwSystemError(__PRETTY_FUNCTION__, message)
   #define THROW_WARNING(message) mcuf::lang::System::throwSystemWarning(__PRETTY_FUNCTION__, message)
-  #define ASSERT_THROW_ERROR(expr, message) if(!expr) THROW_ERROR(message)
-  #define ASSERT_THROW_WARNING(expr, message) if(!expr) THROW_WARNING(message)
+  #define ASSERT_THROW_ERROR(expr, message) if(!(expr)) THROW_ERROR(message)
+  #define ASSERT_THROW_WARNING(expr, message) if(!(expr)) THROW_WARNING(message)
 #else
   #ifdef MCUF_DEBUG_NMSG
     #define INFO(message)
@@ -49,4 +54,4 @@
  */ 
 
 
-#endif/* MCUF_AT32F415_D9A3210A_778E_466F_BA3E_7A53ACD9064E */
+#endif /* MCUF_AT32F415_D9A3210A_778E_466F_BA3E_7A53ACD9064E */
